6c1fb213f9a58d3642c0bffc1a72122b7fbc122b
RAMIL ARSAEV <gattoramm@gmail.com>
2021-08-22 00:18:45 +0300

    --

---
+++ Java-SOLID/src/ITVDN/Part1_Single_Responsibility_Principle/part14_homework/change/Book.java
@@ -0,0 +1,37 @@
+package ITVDN.Part1_Single_Responsibility_Principle.part14_homework.change;
+
+public class Book {
+    private BookPersistence bookPersistence;
+
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
