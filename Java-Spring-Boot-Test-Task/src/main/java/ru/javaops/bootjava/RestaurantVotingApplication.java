568137d87a313b1acae373c17819248ef6a44504
RAMIL ARSAEV <gattoramm@gmail.com>
2021-09-08 22:53:21 +0300

    Добавил заготовку для изучения Spring Boot

---
+++ Java-Spring-Boot-Test-Task/src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
@@ -0,0 +1,13 @@
+package ru.javaops.bootjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantVotingApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantVotingApplication.class, args);
+    }
+
+}
---
+++ Java-Spring-Boot-Test-Task/src/test/java/com/gattoramm/restourantvoting/RestaurantVotingApplicationTest.java
@@ -0,0 +1,13 @@
+package com.gattoramm.restourantvoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantVotingApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
