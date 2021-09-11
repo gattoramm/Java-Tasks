e49bf39cdd89c689684562a38ecffb2284f33804
RAMIL ARSAEV <gattoramm@gmail.com>
2021-09-11 23:45:15 +0300

    Добавление/просмотр пользователей

--- Simple-Web-Project/src/main/java/app/servlets/AddServlet.java
+++ Simple-Web-Project/src/main/java/app/servlets/AddServlet.java
@@ -1,5 +1,8 @@
 package app.servlets;
 
+import app.entities.User;
+import app.model.Model;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,5 +15,17 @@ public class AddServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         RequestDispatcher requestDispatcher = req.getRequestDispatcher("views/add.jsp");
         requestDispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String password = req.getParameter("pass");
+        User user = new User(name, password);
+        Model model = Model.getInstance();
+        model.add(user);
+
+        req.setAttribute("userName", name);
+        doGet(req, resp);
     }
 }
