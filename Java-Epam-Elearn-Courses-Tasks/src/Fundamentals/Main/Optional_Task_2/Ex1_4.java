6d5493a460bc497228e8273eceb6a75bd35c9eee
RAMIL ARSAEV <gattoramm@gmail.com>
2022-01-09 14:54:50 +0300

    добавил пример - поиск суммы элементов матрицы, расположенных первыми 2мя положительными элементами каждой строки

--- Java-Epam-Elearn-Courses-Tasks/src/Fundamentals/Main/Optional_Task_2/Ex1_4.java
+++ Java-Epam-Elearn-Courses-Tasks/src/Fundamentals/Main/Optional_Task_2/Ex1_4.java
@@ -7,8 +7,9 @@ import java.util.Scanner;
  * Задать значения элементов матрицы в интервале значений от -M до M
  * с помощью генератора случайных чисел (класс Random).
  *
- *  Найти сумму элементов матрицы, расположенных между первым и вторым
- *  положительными элементами каждой строки
+ *  Найти сумму элементов матрицы,
+ *  расположенных между первым и вторым положительными элементами
+ *  каждой строки
  */
 
 public class Ex1_4 {
@@ -22,6 +23,43 @@ public class Ex1_4 {
             }
         }
         return matrix;
+    }
+
+    // подсчет 2х положительных элементов в строке
+    public static int sumTwoPositiveValuesMatrixRow(int[] row) {
+        int count = 0;
+        int sum = 0;
+        int j = 0;
+        int[] arrTwoPositiveValues = new int[]{-1, -1};
+
+        for (int i = 0; i < row.length && count < 2; i++) {
+            if (row[i] > 0) {
+                count++;
+                arrTwoPositiveValues[j] = i;
+                j++;
+            }
+        }
+
+        if (arrTwoPositiveValues[0] < 0 && arrTwoPositiveValues[1] < 0)
+            return 0;
+
+
+        for (int i = arrTwoPositiveValues[0]; i < arrTwoPositiveValues[1] - 1; i++)
+            sum += row[i + 1];
+
+        return sum;
+    }
+
+    // вывод суммы элементов матрицы, расположенных первыми 2мя положительными элементами каждой строки
+    public static void printSumTwoPositiveValuesMatrixRow(int[][] matrix) {
+        int[] row = new int[matrix.length];
+
+        for (int out = 0; out < matrix.length; out++) {
+            for (int in = 0; in < matrix[out].length; in++) {
+                row[in] = matrix[out][in];
+            }
+            System.out.println("Row " + out + " : " + sumTwoPositiveValuesMatrixRow(row));
+        }
     }
 
     // распечатка матрицы
@@ -43,5 +81,7 @@ public class Ex1_4 {
 
         printMatrix(matrix);
 
+        System.out.println("Print sum: ");
+        printSumTwoPositiveValuesMatrixRow(matrix);
     }
 }
