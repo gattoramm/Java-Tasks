8f95fc3edc06db4e521b99d18bc2a0f3bcf58b99
RAMIL ARSAEV <gattoramm@gmail.com>
2022-01-27 12:31:15 +0300

    Update Ex1.java

--- Java-Epam-Elearn-Courses-Tasks/src/Fundamentals/Main/Main_Task/Ex1.java
+++ Java-Epam-Elearn-Courses-Tasks/src/Fundamentals/Main/Main_Task/Ex1.java
@@ -1,13 +1,17 @@
 package Fundamentals.Main.Main_Task;
 
+import java.util.Scanner;
+
 /**
  * Приветствовать любого пользователя при вводе его имени через командную строку.
  * */
 
 public class Ex1 {
     public static void main(String[] args) {
-        if(args.length == 0) return;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Insert username: ");
+        String user  = scanner.next();
 
-        System.out.println("Hello " + args[0]);
+        System.out.print("Hello, " + user);
     }
 }
