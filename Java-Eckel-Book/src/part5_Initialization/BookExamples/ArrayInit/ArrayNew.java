7d8d9075f5fac64fc17aec03c3e9d862d8e6922a
RAMIL ARSAEV <gattoramm@gmail.com>
2022-01-27 21:39:05 +0300

    Update ArrayNew.java

--- Java-Eckel-Book/src/part5_Initialization/BookExamples/ArrayInit/ArrayNew.java
+++ Java-Eckel-Book/src/part5_Initialization/BookExamples/ArrayInit/ArrayNew.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public class ArrayNew {
     public static void main(String[] args) {
-        int[] a = new int[new Random().nextInt(20)];;
+        int[] a = new int[new Random().nextInt(20)];
 
         System.out.println("Length of a = " + a.length);
         System.out.println(Arrays.toString(a));
