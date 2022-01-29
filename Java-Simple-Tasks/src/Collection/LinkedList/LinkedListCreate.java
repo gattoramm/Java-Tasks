36f90743ab45d664f7bc10effc0483d63493ac95
RAMIL ARSAEV <gattoramm@gmail.com>
2022-01-29 09:00:03 +0300

    Update LinkedListCreate.java

--- Java-Simple-Tasks/src/Collection/LinkedList/LinkedListCreate.java
+++ Java-Simple-Tasks/src/Collection/LinkedList/LinkedListCreate.java
@@ -6,10 +6,10 @@ public class LinkedListCreate {
 
     public static void main(String[] args) {
 
-        String str1 = new String("Hello World!");
-        String str2 = new String("My name is Earl");
-        String str3 = new String("I love Java");
-        String str4 = new String("I live in Moscow");
+        String str1 = "Hello World!";
+        String str2 = "My name is Earl";
+        String str3 = "I love Java";
+        String str4 = "I live in Moscow";
 
         LinkedList<String> earlBio = new LinkedList<>();
 
