1236c018351c1f27679c60ed87625c86713af701
RAMIL ARSAEV <gattoramm@gmail.com>
2022-02-12 20:11:29 +0300

    добавил заготовку для fuzzbuzz with threads

---
+++ Java-Simple-Tasks/src/Strings/fuzzbuzzWithThreads.java
@@ -0,0 +1,18 @@
+package Strings;
+
+public class fuzzbuzzWithThreads {
+    public static void bingoBongo(int num){
+        StringBuffer result = new StringBuffer("");
+        if (num%3 == 0) result.append("Bingo");
+        if (num%5 == 0) result.append("Bongo");
+        if (num%5 != 0 && num%3 != 0) result.append(num);
+        System.out.println(result);
+    }
+
+    }
+
+    public static void main(String[] args) {
+        for (int i=1; i<101; i++)
+            fuzzbuzzWithThreads.bingoBongo(i);
+    }
+}
