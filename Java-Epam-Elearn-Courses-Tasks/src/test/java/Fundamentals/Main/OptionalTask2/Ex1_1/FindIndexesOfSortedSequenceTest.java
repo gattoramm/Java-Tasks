a47d49ed67482b59118b4f642435447b93a04c12
gattoramm <gattoramm@gmail.ru>
2022-04-22 12:27:48 +0300

    добавил TDD тест, проверяющий индексы отсортированного массива

--- Java-Epam-Elearn-Courses-Tasks/src/test/java/Fundamentals/Main/OptionalTask2/Ex1_1/FindIndexesOfSortedSequenceTest.java
+++ Java-Epam-Elearn-Courses-Tasks/src/test/java/Fundamentals/Main/OptionalTask2/Ex1_1/FindIndexesOfSortedSequenceTest.java
@@ -6,19 +6,37 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FindIndexesOfSortedSequenceTest {
-    private int[] getSortRowsOfMatrixInAsc(int[] sequence) {
+    private int[] getIndexesOfSortedSequence(int[] sequence) {
         return SortRowsOfMatrixInAsc.findIndexesOfSortedSequence(sequence);
     }
 
     @Test
     public void sequenceWithFiveIncreaseElements() {
-        int[] t = {1, 2, 3, 4};
-        assertArrayEquals(new int[]{0, 1, 2, 3}, getSortRowsOfMatrixInAsc(t));
+        int[] t = {1, 2, 3, 4, 999};
+        assertArrayEquals(new int[]{0, 1, 2, 3, 4}, getIndexesOfSortedSequence(t));
     }
 
     @Test
     public void sequenceWithFiveDecreaseElements() {
-        int[] t = {4, 3, 2, 1};
-        assertArrayEquals(new int[]{3, 2, 1, 0}, getSortRowsOfMatrixInAsc(t));
+        int[] t = {4, 3, 2, 1, 7};
+        assertArrayEquals(new int[]{3, 2, 1, 0, 4}, getIndexesOfSortedSequence(t));
+    }
+
+    @Test
+    public void sequenceWithFiveEqualsElements() {
+        int[] t = {-345246, -345246, -345246, -345246, -345246};
+        assertArrayEquals(new int[]{0, 1, 2, 3, 4}, getIndexesOfSortedSequence(t));
+    }
+
+    @Test
+    public void sequenceWithThreeAnyElements() {
+        int[] t = {22, 22, -13};
+        assertArrayEquals(new int[]{2, 0, 1}, getIndexesOfSortedSequence(t));
+    }
+
+    @Test
+    public void sequenceWithSevenAnyElements() {
+        int[] t = {-345246, 345246, -128, -4, -Integer.MIN_VALUE, Integer.MAX_VALUE, (int)Byte.MIN_VALUE};
+        assertArrayEquals(new int[]{4, 0, 2, 6, 3, 1, 5}, getIndexesOfSortedSequence(t));
     }
 }
