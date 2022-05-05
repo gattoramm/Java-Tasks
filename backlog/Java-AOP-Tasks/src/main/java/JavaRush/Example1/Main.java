8d63088038a8de8c662b4b48435917e8dd4a8bc3
gattoramm <gattoramm@gmail.ru>
2022-05-05 09:47:40 +0300

    перевел неактуальное/забытое в беклог

---
+++ backlog/Java-AOP-Tasks/src/main/java/JavaRush/Example1/Main.java
@@ -0,0 +1,13 @@
+package JavaRush.Example1;
+
+public class Main {
+    public static void main(String[] args) {
+        printName("Adam");
+        printName("Sam");
+        printName("John");
+    }
+
+    public static void printName(String name) {
+        System.out.println(name);
+    }
+}
---
+++ backlog/Java-SpringBoot-Test-Tasks/README.md
@@ -0,0 +1,2 @@
+# Java-SpringBoot-Test-Tasks
+ 
