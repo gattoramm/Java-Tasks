bb11f7875107f573059ea33a9554d2dbbae9ea3d
gattoramm <gattoramm@gmail.ru>
2022-05-05 16:26:20 +0300

    добавил пример для логической связности

---
+++ Java-RealWorldSoftwareDevelopmentBook/Java-RealWorldSoftwareDevelopmentBook/src/main/part2/AfterSRP/BankTransactionParser.java
@@ -0,0 +1,18 @@
+package main.part2.AfterSRP;
+
+public class BankTransactionParser {
+    public BankTransaction parseFromCSV(final String line) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransaction parseFromJSON(final String line) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransaction parseFromXML(final String line) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+}
