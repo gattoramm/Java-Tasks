d70be3a7f2526dbdce7fdd5f290b7c6abb5d8a12
gattoramm <gattoramm@gmail.ru>
2022-05-05 16:21:50 +0300

    добавил шаблон примера ДАО

---
+++ Java-RealWorldSoftwareDevelopmentBook/Java-RealWorldSoftwareDevelopmentBook/src/main/part2/AfterSRP/BankTransactionDAO.java
@@ -0,0 +1,25 @@
+package main.part2.AfterSRP;
+
+import java.time.LocalDate;
+
+public class BankTransactionDAO {
+    public BankTransaction create(final LocalDate date, final Double amount, final String description) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransaction read(final long id) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransaction update(final long id) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    public void delete(final BankTransaction bankTransaction) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+}
