d6df0e221fcfc58ba9558d6cb07c6e611f25ac46
gattoramm <gattoramm@gmail.ru>
2022-05-18 11:55:26 +0300

    Вынес проект Java-RealWorldSoftwareDevelopmentBook как отдельный

--- Java-RealWorldSoftwareDevelopmentBook/src/main/java/part2/ex4_AfterCoupling/BankStatementParser.java
+++
@@ -1,9 +0,0 @@
-package part2.ex4_AfterCoupling;
-
-import java.util.List;
-
-// интерфейс для парсинга банковских операций
-public interface BankStatementParser {
-    BankTransaction parseFrom(String line);
-    List<BankTransaction> parseLinesFrom(List<String> lines);
-}
