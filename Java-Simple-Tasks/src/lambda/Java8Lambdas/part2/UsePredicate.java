670bc5bf8d8a8eec66619e9ac4fcbfa705f1210d
gattoramm <gattoramm@gmail.com>
2024-02-15 11:59:28 +0300

    Update UsePredicate.java

--- Java-Simple-Tasks/src/lambda/Java8Lambdas/part2/UsePredicate.java
+++ Java-Simple-Tasks/src/lambda/Java8Lambdas/part2/UsePredicate.java
@@ -5,6 +5,6 @@ import java.util.function.Predicate;
 public class UsePredicate {
     public static void main(String[] args) {
         Predicate<Integer> atLeast5 = x -> x > 5;
-        //System.out.println(atLeast5);
+        System.out.println(atLeast5);
     }
 }
